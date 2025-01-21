4c0a0e442918f240de3dc9bedb68b167f02c5187
Joao Henrique de Freitas Queiroz <jWX1160543@china.huawei.com>
2025-01-21 17:51:11 -0300

    Initial configure, build initial structure for React Study Course

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+const App = () => {
+  // const [count, setCount] = useState(0)
+
+  return (
+    // <>
+    <div>App React</div>
+    // </>
+  );
+};
+
+export default App;
